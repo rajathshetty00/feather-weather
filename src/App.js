3486b30d24da134d747b618ab64e4a93cc5e91ab
rajathshetty00 <rajathshetty00@gmail.com>
2018-11-14 14:36:07 +0530

    commit before final styling

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import "./App.css";
 import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
+import Error from "./components/Error";
 const API_KEYS = "4844304fe2f5e4e608e39b2491dc2c63";
 //const URL = "https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b6907d289e10d714a6e88b30761fae22"
 class App extends Component {
+  state = {
+    city: null,
+    country: null,
+    temperature: null,
+    humidity: null,
+    description: null,
+    error: ""
+  };
+
   getWeather = async event => {
     event.preventDefault();
     var city = event.target.elements.city.value;
@@ -15,6 +25,21 @@ class App extends Component {
     `);
     const data = await api_call.json();
     console.log(data);
+    if (city && country) {
+      this.setState({
+        city: data.name,
+        country: data.sys.country,
+        temperature: data.main.temp,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: null
+      });
+    } else {
+      this.setState({
+        error: "Sorry, I can't find without city and country names "
+      });
+    }
+    console.log(this.state);
   };
 
   render() {
@@ -22,7 +47,14 @@ class App extends Component {
       <div className="App">
         <Titles />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather
+          city={this.state.city}
+          country={this.state.country}
+          temperature={this.state.temperature}
+          humidity={this.state.humidity}
+          description={this.state.description}
+        />
+        <Error error={this.state.error} />
       </div>
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,7 +2,22 @@ import React, { Component } from "react";
 class Weather extends Component {
   state = {};
   render() {
-    return <div>Weather</div>;
+    var person = null;
+
+    if (this.props.city && this.props.country) {
+      person = (
+        <div>
+          <p>
+            Location: {this.props.city},{this.props.country}.
+          </p>
+          <p>Temperature: {this.props.temperature}</p>
+          <p>Humidity: {this.props.humidity}</p>
+          <p> Conditions: {this.props.description}</p>
+          <p>{this.props.error}</p>
+        </div>
+      );
+    }
+    return person;
   }
 }
 
